62e32c1347f9c9d0a97f8a9bd263a0c6fc6b6cdf
liuzhouliang <525688067@qq.com>
2017-10-11 17:31:23 +0800

    q

---
+++ Cglib代理/src/com/test/UserDao.java
@@ -0,0 +1,12 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2017/10/11.
+ *
+ * 目标对象,没有实现任何接口
+ */
+public class UserDao {
+    public void save() {
+        System.out.println("----已经保存数据!----");
+    }
+}
