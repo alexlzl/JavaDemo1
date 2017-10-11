3275a8a2afa7f3c2968bd0f4b5d88a3c11f7d0a5
liuzhouliang <525688067@qq.com>
2017-10-11 17:23:05 +0800

    q

--- 动态代理/src/com/test/BusinessProcessorHandler.java
+++ 动态代理/src/com/test/BusinessProcessorHandler.java
@@ -5,6 +5,8 @@ import java.lang.reflect.Method;
 
 /**
  * Created by liuzhouliang on 2017/10/11.
+ *
+ * http://www.cnblogs.com/cenyu/p/6289209.html
  */
 public class BusinessProcessorHandler implements InvocationHandler {
 
@@ -14,16 +16,27 @@ public class BusinessProcessorHandler implements InvocationHandler {
         this.target = target;
     }
 
+    /**
+     * ClassLoader loader,:指定当前目标对象使用类加载器,获取加载器的方法是固定的
+     * Class<?>[] interfaces,:目标对象实现的接口的类型,使用泛型方式确认类型
+     * InvocationHandler h:事件处理,执行目标对象的方法时,会触发事件处理器的方法,会把当前执行目标对象的方法作为参数传入
+     *
+     * @param proxy
+     * @param method
+     * @param args
+     * @return
+     * @throws Throwable
+     */
     @Override
     public Object invoke(Object proxy, Method method, Object[] args)
             throws Throwable {
-        if(method.getName().equals("test")){
+        if (method.getName().equals("test")) {
             System.out.println("==test 方法===");
         }
-        if(method.getName().equals("test1")){
+        if (method.getName().equals("test1")) {
             System.out.println("==test1 方法===");
         }
-        if(method.getName().equals("processBusiness")){
+        if (method.getName().equals("processBusiness")) {
             System.out.println("==processBusiness 方法===");
         }
         System.out.println("You can do something here before process your business");
--- 动态代理/src/com/test/Test.java
+++ 动态代理/src/com/test/Test.java
@@ -8,6 +8,16 @@ import java.lang.reflect.Proxy;
 /**
  * Created by liuzhouliang on 2017/10/11.
  * <p>
+ * CGLib创建的动态代理对象性能比JDK创建的动态代理对象的性能高不少，但是CGLib在创建代理对象时所花费的时间却比JDK多得多，所以对于单例的对象，因为无需频繁创建对象，用CGLib合适，反之，使用JDK方式要更为合适一些。同时，由于CGLib由于是采用动态创建子类的方法，对于final方法，无法进行代理！
+ *
+ * 动态代理有以下特点:
+ * 1.代理对象,不需要实现接口
+ * 2.代理对象的生成,是利用JDK的API,动态的在内存中构建代理对象(需要我们指定创建代理对象/目标对象实现的接口的类型)
+ * 3.动态代理也叫做:JDK代理,接口代理
+ * <p>
+ * 总结:
+ * 代理对象不需要实现接口,但是目标对象一定要实现接口,否则不能用动态代理
+ * <p>
  * 1，根据传入的第二个参数interfaces动态生成一个类，实现interfaces中的接口，该例中即BusinessProcessor接口的processBusiness方法。并且继承了Proxy类，重写了hashcode,toString,equals等三个方法。具体实现可参看 ProxyGenerator.generateProxyClass(...); 该例中生成了$Proxy0类
  * <p>
  * 2，通过传入的第一个参数classloder将刚生成的类加载到jvm中。即将$Proxy0类load
