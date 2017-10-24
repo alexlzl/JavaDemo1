8ca36095dc25af5293dcc41133431fbc614c1d55
liuzhouliang <525688067@qq.com>
2017-10-24 16:09:36 +0800

    q

---
+++ StringFormat/src/com/test/Test.java
@@ -0,0 +1,10 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2017/10/24.
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.println(String.format("domain=%s", new Object[]{"test","one"}));
+    }
+}
