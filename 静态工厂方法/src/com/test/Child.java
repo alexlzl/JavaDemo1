e2feb92556d80ac1cf8fac1b0477fed31a448f29
liuzhouliang <525688067@qq.com>
2018-01-24 11:14:31 +0800

    q

---
+++ 静态工厂方法/src/com/test/Child.java
@@ -0,0 +1,11 @@
+package com.test;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+/**
+ * Created by liuzhouliang on 2018/1/23.
+ */
+public class Child extends Test {
+
+}
---
+++ 静态工厂方法/src/com/test/Test.java
@@ -0,0 +1,14 @@
+package com.test;
+
+/**
+ * Created by liuzhouliang on 2018/1/23.
+ */
+public class Test extends  Parent{
+    private void Test(){
+
+    }
+
+    public static Test getInstance(){
+        return  new Test();
+    }
+}
